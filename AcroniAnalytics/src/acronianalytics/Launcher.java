bbf4bffad0d2350db609bab762447cc898427d8a
felipisan <gmininsk@gmail.com>
2019-07-01 23:49:37 -0300

    Eae Login tá BONITO??

---
+++ AcroniAnalytics/src/acronianalytics/Launcher.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acronianalytics;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author iwakura
+ */
+public class Launcher extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/acronianalytics/views/login.fxml"));
+        
+        //Esperando o dia em que o Braga terminar os layouts rs
+        primaryStage.setTitle("Shit is crazy shit is yaeji");
+        primaryStage.initStyle(StageStyle.UTILITY);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
